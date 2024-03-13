36a7b15ab97d9000cd1c2a1a847beb82e1b15118
Mattheus Cassundé <mattheus.089@gmail.com>
2024-03-13 07:21:27 -0300

    thymeleaf

--- src/main/java/br/com/cassunde/thymeleaf/PDF.java
+++ src/main/java/br/com/cassunde/thymeleaf/PDF.java
@@ -1,12 +1,10 @@
-package br.com.cassunde.services;
+package br.com.cassunde.thymeleaf;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.thymeleaf.TemplateEngine;
@@ -16,8 +14,6 @@ import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import org.xhtmlrenderer.pdf.ITextRenderer;
 
 import com.lowagie.text.DocumentException;
-
-import br.com.cassunde.thymeleaf.Author;
 
 public class PDF {
 
